8657c1bff9f93645c034e5e8d6d30df74f418dc6
Erhahon Imafidon <mafiosco4@gmail.com>
2024-09-01 06:23:25 +0100

    Testing the husky hook

--- App.tsx
+++ App.tsx
@@ -2,20 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Text>Just tested it on Android</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const dogName = 'Jungle';
+
+    console.log(dogName);
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <Text>Just tested it on Android</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
